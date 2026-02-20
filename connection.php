31a2356a925677edd70f1e178a4ac989fda9cb13
ljuban-jajcanin <ljuban.jajcanin@hostanno.com>
2026-02-20 21:38:26 +0100

    connection revert

--- connection.php
+++ connection.php
@@ -1,11 +1,5 @@
 <?php
 
-//AVOID DIRECT ACCESS TO FILE
-if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {        
-    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-    die( header( 'location: 404.php' ) );
-}
-
 include_once('cred.php');
 
 try{
